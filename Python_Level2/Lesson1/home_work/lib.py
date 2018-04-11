77866e22ca673790865c82e0ebaff067cc0d1be9
meSergie <svetlovsv@gmail.com>
2018-04-11 20:13:22 +0300

    Add return for server and client main loop func for testing.

--- Python_Level2/Lesson1/home_work/lib.py
+++ Python_Level2/Lesson1/home_work/lib.py
@@ -12,17 +12,18 @@ def main_loop_for_client(sock):
             "account_name": "C0deMaver1ck",
             "status": "Yep, I am here!"
             }
-
     }
-
-    message_json = json.dumps(message)
-    message_buf = message_json.encode()
 
     resp = sock.recv(1024).decode()
     resp_json = json.loads(resp)
-    print(resp_json)
+    #print(resp_json)
     if resp_json['action'] == 'probe':
+        
+        message_json = json.dumps(message)
+        message_buf = message_json.encode()
         sock.send(message_buf)
+    
+    return resp_json['action'], message['action']
     
 def main_loop_for_server(sock):
 
@@ -38,14 +39,18 @@ def main_loop_for_server(sock):
     
     resp = sock.recv(1024)
     resp_json = json.loads(resp)
-    print(resp_json)
-    if resp_json['action'] == 'presence':
-        message =\
-        {
-            "response": 200,
-            "time": time.time(),
-            "alert": "OK"
-        }
-        message_json = json.dumps(message)
-        message_buf = message_json.encode()
-        sock.send(message_buf)
+    # print(resp_json)
+    
+    result = resp_json['action'], message['action']
+
+    return result
+    # if resp_json['action'] == 'presence':
+    #     message =\
+    #     {
+    #         "response": 200,
+    #         "time": time.time(),
+    #         "alert": "OK"
+    #     }
+    #     message_json = json.dumps(message)
+    #     message_buf = message_json.encode()
+    #     sock.send(message_buf)
